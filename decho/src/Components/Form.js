75056e716ef5f68e80e5f5bc6693a0c9ef2be698
CalgoDex <goah.calvin96@gmail.com>
2020-06-13 18:09:46 -0400

    created a simple form, still need to do some editing to make it look nice

--- decho/src/Components/Form.js
+++ decho/src/Components/Form.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
-import { Timeline } from 'react-twitter-widgets';
 
 export default class UserForm extends Component {
-  state = {
-    username: null,
-    politicalLean: null,
-  };
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      age: null,
+    };
+  }
 
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    alert("You are submitting " + this.state.username);
+  }
+
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
   render() {
     return (
-      <div style={{ border: '1px solid blue' }}>
-        <form action="">
-          <input type="text" value={this.state.username} /> <br />
-          <input type="text" value={this.state.politicalLean} /> <br />
-        </form>
-      </div>
+      <form onSubmit={this.mySubmitHandler}>
+      <h1>Hello {this.state.username} {this.state.age}</h1>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        name='username'
+        onChange={this.myChangeHandler}
+      />
+      <p>Enter your age:</p>
+      <input
+        type='text'
+        name='age'
+        onChange={this.myChangeHandler}
+      />
+      <select onChange={this.myChangeHandler}>
+        <option value="Ford">Ford</option>
+        <option value="Volvo" defaultValue>Volvo</option>
+        <option value="Fiat">Fiat</option>
+      </select>
+      <input
+        type='submit'
+      />
+      </form>
     );
   }
 }
