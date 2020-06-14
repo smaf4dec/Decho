de73e068b7d8905ecd3f7104742c29cd80b79350
arianna choza <unachoza@gmail.com>
2020-06-14 19:32:42 -0400

    putting this branch away

--- decho/src/Pages/Landing/LandingPage.js
+++ decho/src/Pages/Landing/LandingPage.js
@@ -4,13 +4,12 @@ import 'Pages/Landing/LandingPage.styles.css';
 import { ReactComponent as DeChoLogo } from 'assets/logo-big.svg';
 // import './LandingPage.styles.css';
 // import UserForm from '../Components/Form/Form';
-/*<div id="de-echo-to" className="title">De-Echo</div>*/
+
 export default class LandingPage extends Component {
   render() {
     return (
       <div>
         <div className="text-constiner">Minimize your social media echo chambers & Engage in healthy interactions</div>
-
         <div className="logo">
           <DeChoLogo id="megaphonoLogo" />
         </div>
