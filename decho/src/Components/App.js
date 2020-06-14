23db97bcd861d15cd3f8712080d17ba6a305a779
unachoza <unachoza@gmail.com>
2020-06-13 23:45:10 -0400

    Revert "folder structure"

--- decho/src/Components/App.js
+++ decho/src/Components/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-import 'Components/App.css';
-import LandingPage from 'Pages/Landing//LandingPage';
-import TimelinePage from 'Pages/Timeline/TimelinePage';
-import { getOneOppFromServer } from 'Pages/partisans/getOpp';
-// import './App.css';
-// import LandingPage from '../Pages/Landing//LandingPage';
-// import TimelinePage from '../Pages/Timeline/TimelinePage';
-// import { getOneOppFromServer } from '../Pages/partisans/getOpp';
+import '../App.css';
+import LandingPage from '../Pages/LandingPage';
+import TimelinePage from '../Pages/TimelinePage';
+import {getOneOppFromServer} from '../Pages/partisans/getOpp'
 
 class App extends Component {
   state = {
@@ -14,29 +10,26 @@ class App extends Component {
     currentLean: 'right',
     opp: '',
   };
-
-  //this is a closure / work around to aviod using redux
-  //this can totally be refactored
-
+  
   changePage = (page) => {
-    return (currentLean) => {
-      getOneOppFromServer(currentLean).then((opp) => {
+   return (currentLean) =>{
+
+    getOneOppFromServer(currentLean)
+      .then((opp) => {
         this.setState({
           currentPage: page,
-          currentLean: currentLean || this.state.currentLean,
-          opp,
-        });
-      });
-    };
-  };
+          currentLean: currentLean || this.state.currentLean ,
+          opp
+        })
+      })
+   }
+  }
   render() {
     const { currentPage } = this.state;
     return (
       <div className="App">
-        {currentPage === 'landing' && <LandingPage changePage={this.changePage('timeline')} />}
-        {currentPage === 'timeline' && (
-          <TimelinePage politicalLean={this.state.currentLean} partisan={this.state.opp} />
-        )}
+        {currentPage === 'landing' && <LandingPage changePage={ this.changePage('timeline') } />}
+        {currentPage === 'timeline' && <TimelinePage politicalLean={this.state.currentLean} partisan={this.state.opp}/>}
       </div>
     );
   }
