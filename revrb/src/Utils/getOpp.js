69188146a1801df959db2d22ab97af88daa1eec9
CalgoDex <goah.calvin96@gmail.com>
2020-07-14 00:52:05 -0400

    commiting changes that will be undone. GitHub does not track file name changes properly at times

--- revrb/src/Utils/getOpp.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const API = "https://deco-db.herokuapp.com/users"
-
-export const getOneOppFromServer = async() => {
-    const {data} = await axios.get(API);
-    let oneOpp = data[Math.floor(Math.random() * data.length)]
-    return oneOpp;
-}
